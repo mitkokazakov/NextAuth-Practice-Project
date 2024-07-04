43fcb0c55669757f9acfa377789f03e3f37488f8
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2024-07-04 15:30:03 +0300

    AuthOptions added

--- nextauth-practice/app/api/auth/[...nextauth]/route.ts
+++ nextauth-practice/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,28 @@
 import NextAuth from "next-auth/next";
+import { AuthOptions } from "next-auth";
 import prisma from "@/app/lib/prismadb";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { CredentialsProvider } from "next-auth/providers/credentials";
-import { GoogleProfile } from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: AuthOptions = {
+
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+        }),
+        CredentialsProvider({
+            name: "credentials",
+            credentials: {
+                email: { label: "Email", type: "text", placeholder: "jsmith@yahoo.com" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials){
+                const user = {id: 1, email: "testmail@yahoo.com"} 
+                return user;
+            }
+        })
+    ]
+}

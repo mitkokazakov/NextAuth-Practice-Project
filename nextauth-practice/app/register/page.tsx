450767bf92e0ef21304fff853c412d334639ee3e
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2024-08-02 13:16:00 +0300

    Protecting routes

--- nextauth-practice/app/register/page.tsx
+++ nextauth-practice/app/register/page.tsx
@@ -5,16 +5,15 @@ import { redirect, useRouter } from 'next/navigation'
 import { useSession } from "next-auth/react";
 import RegisterForm from "../components/RegisterForm";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
 
 
 const page = async () => {
 
-  const session = await getServerSession(authOptions)
+  // const session = await getServerSession(authOptions)
 
-  if(session){
-    redirect("/");
-  }
+  // if(session){
+  //   redirect("/");
+  // }
 
 
   return (
